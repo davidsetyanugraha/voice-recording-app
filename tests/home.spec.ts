d0ce07f997bef238bf369e4fd8df143c1f4c35ef
David <dsetyanugraha@gmail.com>
2025-02-28 16:25:29 +1100

    Add e2e test

--- tests/home.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("App should always have correct title", async ({ page }) => {
-  await page.goto("http://localhost:5173");
-  await expect(page).toHaveTitle(/Voice recording app/i);
-});
