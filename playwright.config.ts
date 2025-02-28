d32a8781ee18102f187a5398abd0ab19c5d3ab9c
David <dsetyanugraha@gmail.com>
2025-02-28 15:38:57 +1100

    Add playwright test

---
+++ playwright.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  webServer: {
+    command: "pnpm run dev",
+    port: 5173, // Default Vite port
+    reuseExistingServer: true,
+  },
+});
