308cd6ee7fc45c1c3c3ac5ca5b2fe4921f2e96a6
David <dsetyanugraha@gmail.com>
2025-02-28 15:43:21 +1100

    Fix jest and playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests",
+  testMatch: ["**/*.spec.ts"],
   webServer: {
     command: "pnpm run dev",
     port: 5173, // Default Vite port
